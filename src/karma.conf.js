730f09e925b2ac9819565b324084e1e2953cb5db
Deivid Fortuna <deividfortuna@gmail.com>
2021-07-08 16:59:39 +1200

    fix test of project

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -9,7 +9,8 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
+      require('karma-coveralls'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
